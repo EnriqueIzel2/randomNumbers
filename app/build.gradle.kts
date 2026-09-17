e9b9bd4bc49358523319ca0cc030304ef8106586
enrique-izel-sns <eleaoizel@gmail.com>
2026-09-16 20:22:02 -0400

    feat: create the result and sort screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
